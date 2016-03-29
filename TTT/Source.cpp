4db6893056abf52b4a7a06abbec5f157c05b45fc
UlikaNaidoo <ulika.d.naidoo@gmail.com>
2016-03-29 13:06:53 +0200

    Not registering moves

--- TTT/Source.cpp
+++ TTT/Source.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class TTT
 {
 private:
-	int array[3] [3];
+	char array[3] [3];
 	int playtracker;
 
 public:
@@ -19,20 +19,24 @@ public:
 void TTT::resetarray()
 {
 	cout << "A dash '-' means the block is unused." << endl;
-	array[3][3] == { {"-", "-", "-" }; { "-", "-", "-" }; { "-", "-", "-" } };
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++){
+			array[i][j] = '-';
+		}
+	}
 	playtracker = 1; // if this equals 1 then player 1's turn, if equals 2 then player 2s turn.
 
 };
 
 void TTT::print()
 {
-	array[3][3];
 	for (int i = 0; i < 3; i++){
 		for (int j = 0; j < 3; j++){
 			cout << array[i][j] << "  ";
 		}
 		cout << endl;
 	}
+
 };
 
 bool TTT::move(int row, int col)
@@ -47,17 +51,17 @@ bool TTT::move(int row, int col)
 		ans = false;
 	}if (col > 2){
 		ans = false;
-	}if (array[row][col] != "-"){
+	}if (array[row][col] != '-'){
 		ans = false;
 	}
 	else {
 		if (playtracker == 1){
-			array[row][col] == "X";
-			playtracker== 2
+			array[row][col] == 'X';
+			playtracker == 2;
 		}
-		if (playtracker == 2){
-			array[row][col] == "O";
-			playtracker == 1
+		else if (playtracker == 2) {
+			array[row][col] == 'O';
+			playtracker == 1;
 		}		
 	}
 
@@ -86,77 +90,77 @@ int TTT::iswon()
 	int ans = 3; //1-Player 1 won, 2- Player 2 won, 3- Draw
 
 			
-	if (array[0][0] == array[0][1] && array[0][0] == array[0][2] && array[0][0] != "-"){
-		if (array[0][0] == "X"){
+	if (array[0][0] == array[0][1] && array[0][0] == array[0][2] && array[0][0] != '-'){
+		if (array[0][0] == 'X'){
 			ans = 1;
 		}
-		if (array[0][0] == "0"){
+		if (array[0][0] == '0'){
 			ans = 2;
 		}
 
 	}
 					
-	if (array[1][0] == array[1][1] && array[1][0] == array[1][1] && array[1][0] != "-"){
-		if (array[1][0] == "X"){
+	if (array[1][0] == array[1][1] && array[1][0] == array[1][1] && array[1][0] != '-'){
+		if (array[1][0] == 'X'){
 			ans = 1;
 		}
-		if (array[1][0] == "0"){
+		if (array[1][0] == '0'){
 			ans = 2;
 		}
 	}
 
-	if (array[2][0] == array[2][1] && array[2][0] == array[2][2] && array[2][0] != "-"){
-		if (array[2][0] == "X"){
+	if (array[2][0] == array[2][1] && array[2][0] == array[2][2] && array[2][0] != '-'){
+		if (array[2][0] == 'X'){
 			ans = 1;
 		}
-		if (array[2][0] == "0"){
+		if (array[2][0] == '0'){
 			ans = 2;
 		}
 	}
 
 			
-	if (array[0][0] == array[0][1] && array[0][0] == array[0][2] && array[0][0] != "-"){
-		if (array[0][0] == "X"){
+	if (array[0][0] == array[0][1] && array[0][0] == array[0][2] && array[0][0] != '-'){
+		if (array[0][0] == 'X'){
 			ans = 1;
 		}
-		if (array[0][0] == "0"){
+		if (array[0][0] == '0'){
 			ans = 2;
 		}
 	}
 			
-	if (array[1][0] == array[1][1] && array[1][0] == array[1][2] && array[1][0] !=  "-"){
-		if (array[1][0] == "X"){
+	if (array[1][0] == array[1][1] && array[1][0] == array[1][2] && array[1][0] !=  '-'){
+		if (array[1][0] == 'X'){
 			ans = 1;
 		}
-		if (array[1][0] == "0"){
+		if (array[1][0] == '0'){
 			ans = 2;
 		}
 	}
 
 
-	if (array[2][0] == array[2][1] && array[2][0] == array[2][2] && array[2][0] != "-"){
-		if (array[2][0] == "X"){
+	if (array[2][0] == array[2][1] && array[2][0] == array[2][2] && array[2][0] != '-'){
+		if (array[2][0] == 'X'){
 			ans = 1;
 		}
-		if (array[2][0] == "0"){
+		if (array[2][0] == '0'){
 			ans = 2;
 		}
 	}
 			
-	if (array[0][0] == array[1][1] && array[0][0] == array[2][2] && array[0][0] !=  "-"){
-		if (array[0][0] == "X"){
+	if (array[0][0] == array[1][1] && array[0][0] == array[2][2] && array[0][0] !=  '-'){
+		if (array[0][0] == 'X'){
 			ans = 1;
 		}
-		if (array[0][0] == "0"){
+		if (array[0][0] == '0'){
 			ans = 2;
 		}
 	}
 			
-	if (array[2][2] == array[1][1] && array[2][2] == array[1][1] && array[2][2] != "-"){
-		if (array[2][2] == "X"){
+	if (array[0][2] == array[1][1] && array[0][2] == array[1][2] && array[0][2] != '-'){
+		if (array[2][2] == 'X'){
 			ans = 1;
 		}
-		if (array[2][2] == "0"){
+		if (array[2][2] == '0'){
 			ans = 2;
 		}
 	}
@@ -172,31 +176,38 @@ int main()
 	
 	TTT tttobject;
 	cout << "You're playing TicTacToe" << endl;
+	cout << endl;
 	tttobject.resetarray();
 	tttobject.print();
 
 	int counter = 1;
-	while (counter <= 9 && tttobject.iswon != 3){
+	while (counter <= 9 && tttobject.iswon() == 3){
 		cout << "Player enter the row number:" << endl;
 		cin >> row;
 		cout << "Player enter the column number:" << endl;
 		cin >> col;
 		tttobject.move(row, col);
 		if (tttobject.move(row, col) == true){
+			tttobject.move(row, col);
 			counter += 1;
 			tttobject.print();
+			cout << endl;
 		}
 		else {
 			cout << "Invalid numbers" << endl;
 		};
 	};
 
-	tttobject.iswon() == 1;
-	cout << "Player 1 has won" << endl;
-	tttobject.iswon() == 2;
-	cout << "Player 2 has won" << endl;
-	tttobject.iswon() == 3;
-	cout << "The match is a draw" << endl;
+	if (tttobject.iswon() == 1){
+		cout << "Player 1 has won" << endl;
+	}
+	if (tttobject.iswon() == 2){
+		cout << "Player 2 has won" << endl;
+	}
+	if (tttobject.iswon() == 3){
+		cout << "The match is a draw" << endl;
+	}
+	
 	
 	tttobject.resetarray();
 		
